a6c43cb423521200a9b27029dab5c03eeea0aedd
leandromenoci <menoci.leandro@gmail.com>
2023-11-02 23:57:28 -0300

    Tratamento de erro

---
+++ src/Excecao/personalizadaA/Validar.java
@@ -0,0 +1,21 @@
+package Excecao.personalizadaA;
+
+import Excecao.Aluno;
+
+public class Validar {
+    private Validar()  {}
+
+    public static void aluno(Aluno aluno) {
+        if(aluno == null) {
+            throw new IllegalArgumentException("O Aluno está nulo");
+        }
+
+        if(aluno.nome == null || aluno.nome.trim().isEmpty()) {
+            throw new StringVaziaException("nome");
+        }
+
+        if(aluno.nota < 0 || aluno.nota > 10) {
+            throw new NumeroForaIntervaloException("nota");
+        }
+    }
+}
---
+++ src/Excecao/personalizadaB/personalizadaA/TesteValidacao.java
@@ -0,0 +1,20 @@
+package Excecao.personalizadaB.personalizadaA;
+
+import Excecao.Aluno;
+
+public class TesteValidacao {
+    public static void main(String[] args) {
+
+        try {
+            Aluno aluno = new Aluno("", 7);
+            Validar.aluno(aluno);
+
+            Validar.aluno(null);
+        } catch (StringVaziaException | NumeroForaIntervaloException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        System.out.println("Fim");
+    }
+}
