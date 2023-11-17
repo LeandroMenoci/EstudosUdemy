4dba9fc1a47192e8a83f7c4887eb860bacffea42
leandromenoci <menoci.leandro@gmail.com>
2023-11-17 14:39:24 -0300

    Observer

---
+++ src/JDBC/TesteConexao.java
@@ -0,0 +1,7 @@
+package JDBC;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Padroes/Observer/ObservadorChegadaAniversariante.java
+++
@@ -1,6 +0,0 @@
-package Padroes.Observer;
-
-public interface ObservadorChegadaAniversariante {
-
-    void chegou(EventoChegadaAniversariante evento);
-}
